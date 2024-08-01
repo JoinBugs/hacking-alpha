bebefaeb4ac7bfb3f1a74bd5eef18fa785b27b72
Andres <cornejo.cruz.andres@gmail.com>
2024-08-01 12:20:22 -0400

    feat: added new url scope and enable interactions on main content

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,16 @@
-const qcss_dialog = '.z-modal[role="dialog"]';
-const qcss_body   = 'scrollLock';
+const qcss_dialog = '.z-modal[role="dialog"]',
+      qcss_body   = 'scrollLock',
+      qcss_content= 'content';
 
 (function checkForDialog(){
   setTimeout(function(){
     const dialog = document.querySelector(qcss_dialog);
 
     if(dialog) {
-      const body = document.body;
+      const body    = document.body,
+            content = document.getElementById(qcss_content);
+
+      content.removeAttribute('inert');
       body.classList.remove(qcss_body);
       body.style.overflow='scroll';
       dialog.style.display='none';
